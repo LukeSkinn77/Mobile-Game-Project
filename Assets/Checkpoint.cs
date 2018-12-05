85445ff999b871e35fe5f1feb5240159fed99485
BlahBlah77 <k1623759@kingston.ac.uk>
2018-12-05 13:06:42 +0000

    Checkpoints

---
+++ Assets/Checkpoint.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour {
+
+    public void Interaction ()
+    {
+        Game_Manager.Instance.savedPlayerLocation = this.transform.position;
+        Game_Manager.Instance.SavePlayer();
+        GameObject particle = Pickups_Particle_Pooling.pickupPool.GetPickupParticle();
+        if (particle == null) return;
+        particle.transform.position = this.transform.position;
+        particle.transform.rotation = this.transform.rotation;
+        particle.SetActive(true);
+        gameObject.SetActive(false);
+    }
+}
